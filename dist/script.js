ed71f4804bf6854db1f48c9c5c37b68ea4f7516c
Akshay Jangir <52463851+axy1976@users.noreply.github.com>
2021-12-11 23:35:55 +0530

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,137 @@
+let username = 'ABC';
+let isLoggedIn = false;
+
+function CheckLogin() {
+  return /*#__PURE__*/(
+    React.createElement("div", null, /*#__PURE__*/
+    React.createElement("div", null, /*#__PURE__*/
+    React.createElement("div", { class: "container-fluid jumbotron rounded-0 text-center bg-colord mb-0" }, /*#__PURE__*/
+    React.createElement("h1", { class: "colorl" }, "e-commerce cart")), /*#__PURE__*/
+
+    React.createElement("div", { class: "bg-colorl" }, /*#__PURE__*/
+    React.createElement("nav", { class: "container navbar navbar-expand-lg navbar-light colord bg-transparent" }, /*#__PURE__*/
+    React.createElement("button", { class: "navbar-toggler", type: "button", "data-toggle": "collapse", "data-target": "#navbarSupportedContent", "aria-controls": "navbarSupportedContent", "aria-expanded": "false", "aria-label": "Toggle navigation" }, /*#__PURE__*/
+    React.createElement("span", { class: "navbar-toggler-icon" })), /*#__PURE__*/
+
+
+    React.createElement("div", { class: "collapse navbar-collapse", id: "navbarSupportedContent" }, /*#__PURE__*/
+    React.createElement("ul", { class: "navbar-nav mr-auto" }, /*#__PURE__*/
+    React.createElement("li", { class: "nav-item" }, /*#__PURE__*/
+    React.createElement("a", { class: "nav-link", href: "#" }, "Home")), /*#__PURE__*/
+
+    React.createElement("li", { class: "nav-item active" }, /*#__PURE__*/
+    React.createElement("a", { class: "nav-link", href: "#" }, "Products")), /*#__PURE__*/
+
+    React.createElement("li", { class: "nav-item" }, /*#__PURE__*/
+    React.createElement("a", { class: "nav-link", href: "#" }, "About us")), /*#__PURE__*/
+
+    React.createElement("li", { class: "nav-item" }, /*#__PURE__*/
+    React.createElement("a", { class: "nav-link", href: "#" }, "Contacts"))), /*#__PURE__*/
+
+
+    React.createElement("div", { class: "form-inline my-2 my-lg-0" }, /*#__PURE__*/
+    React.createElement("a", { href: "", class: "colord mr-4" }, /*#__PURE__*/React.createElement("b", null, "Cart")), /*#__PURE__*/
+    React.createElement("button", { class: "btn bg-colord colorl btn-md" }, "Logout")))))), /*#__PURE__*/
+
+
+
+
+
+    React.createElement("div", { class: "container-fluid content bg-colord" }, /*#__PURE__*/
+    React.createElement("div", { class: "container p-5 colorl" }, /*#__PURE__*/
+    React.createElement("h5", null, "All Products"), /*#__PURE__*/
+    React.createElement("div", { class: "m-3 row" }, /*#__PURE__*/
+    React.createElement("div", { class: "col-md-9" }, /*#__PURE__*/
+    React.createElement("div", { class: "row" }, /*#__PURE__*/
+    React.createElement("div", { class: "card col-md-3 m-4" }, /*#__PURE__*/
+    React.createElement("div", { class: "card-image" }, /*#__PURE__*/
+    React.createElement("img", { src: "https://m.media-amazon.com/images/I/51FKBDpXEqL.jpg", height: "180", width: "100%" })), /*#__PURE__*/
+
+    React.createElement("div", { class: "card-header text-center colord bg-transparent" }, "Aurdino (UNO)"), /*#__PURE__*/
+
+
+    React.createElement("button", { class: "btn bg-colord colorl mb-2" }, "Add To Cart")), /*#__PURE__*/
+
+
+    React.createElement("div", { class: "card col-md-3 m-4" }, /*#__PURE__*/
+    React.createElement("div", { class: "card-image" }, /*#__PURE__*/
+    React.createElement("img", { src: "https://m.media-amazon.com/images/I/81xhxQ7qCbL._SX679_.jpg", height: "180", width: "100%" })), /*#__PURE__*/
+
+    React.createElement("div", { class: "card-header text-center colord bg-transparent" }, "NIC"), /*#__PURE__*/
+
+
+    React.createElement("button", { class: "btn bg-colord colorl mb-2" }, "Add To Cart")), /*#__PURE__*/
+
+
+    React.createElement("div", { class: "card col-md-3 m-4" }, /*#__PURE__*/
+    React.createElement("div", { class: "card-image" }, /*#__PURE__*/
+    React.createElement("img", { src: "https://m.media-amazon.com/images/I/61-0HSm+zeL._SL1200_.jpg", height: "180", width: "100%" })), /*#__PURE__*/
+
+    React.createElement("div", { class: "card-header text-center colord bg-transparent" }, "Cabinet"), /*#__PURE__*/
+
+
+    React.createElement("button", { class: "btn bg-colord colorl mb-2" }, "Add To Cart")), /*#__PURE__*/
+
+
+    React.createElement("div", { class: "card col-md-3 m-4" }, /*#__PURE__*/
+    React.createElement("div", { class: "card-image" }, /*#__PURE__*/
+    React.createElement("img", { src: "https://m.media-amazon.com/images/I/818fDro1x9L._SL1500_.jpg", height: "180", width: "100%" })), /*#__PURE__*/
+
+    React.createElement("div", { class: "card-header text-center colord bg-transparent" }, "Motherboard"), /*#__PURE__*/
+
+
+    React.createElement("button", { class: "btn bg-colord colorl mb-2" }, "Add To Cart")), /*#__PURE__*/
+
+
+    React.createElement("div", { class: "card col-md-3 m-4" }, /*#__PURE__*/
+    React.createElement("div", { class: "card-image" }, /*#__PURE__*/
+    React.createElement("img", { src: "https://m.media-amazon.com/images/I/51p2d9NML5L._SL1000_.jpg", height: "180", width: "100%" })), /*#__PURE__*/
+
+    React.createElement("div", { class: "card-header text-center colord bg-transparent" }, "Router"), /*#__PURE__*/
+
+
+    React.createElement("button", { class: "btn bg-colord colorl mb-2" }, "Add To Cart")))), /*#__PURE__*/
+
+
+
+    React.createElement("div", { class: "col-md-3 mt-4" }, /*#__PURE__*/
+    React.createElement("div", { class: "rounded-2 card p-2 colord" }, /*#__PURE__*/
+    React.createElement("h5", null, "Cart"), /*#__PURE__*/
+    React.createElement("div", { class: "cart-menu border border-" }), /*#__PURE__*/
+
+
+    React.createElement("div", { class: "border-0 border-bottom-1" }, /*#__PURE__*/
+    React.createElement("table", null, /*#__PURE__*/
+    React.createElement("tr", null, /*#__PURE__*/
+    React.createElement("td", null, "Gross Amount"), /*#__PURE__*/
+    React.createElement("td", null, ":"), /*#__PURE__*/
+    React.createElement("td", null, "0")), /*#__PURE__*/
+
+    React.createElement("tr", null, /*#__PURE__*/
+    React.createElement("td", null, "Tax Amount"), /*#__PURE__*/
+    React.createElement("td", null, ":"), /*#__PURE__*/
+    React.createElement("td", null, "0")), /*#__PURE__*/
+
+    React.createElement("tr", null, /*#__PURE__*/
+    React.createElement("td", null, "Shipping Amount"), /*#__PURE__*/
+    React.createElement("td", null, ":"), /*#__PURE__*/
+    React.createElement("td", null, "0"))))))))), /*#__PURE__*/
+
+
+
+
+
+
+
+
+    React.createElement("div", { class: "p-3 text-center bg-colorl" }, /*#__PURE__*/
+    React.createElement("span", null, "Assignment 7 [ Akshay Jangir ] "), /*#__PURE__*/
+    React.createElement("span", { class: "bg-colord p-2 rounded" }, "\uD83D\uDE01 \uD83D\uDE0E \uD83D\uDD25"))));
+
+
+
+}
+
+ReactDOM.render( /*#__PURE__*/
+React.createElement(CheckLogin, null),
+document.getElementById('root'));
