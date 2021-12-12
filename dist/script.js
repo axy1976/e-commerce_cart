6d06a34429791c0a1b1ce14c075ae461d13c1fd9
Akshay Jangir <52463851+axy1976@users.noreply.github.com>
2021-12-12 16:00:12 +0530

    updated

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,141 @@
-let username = 'ABC';
-let isLoggedIn = false;
+function Cartx() {
+  React.useEffect(() => {});
+  let totalamount = 0;
+  let cartItems = 0;
+  const [total, setTotal] = React.useState({
+    totalQty: 0,
+    totalPrice: 0 });
 
-function CheckLogin() {
+  const [data, setdata] = React.useState([
+  {
+    productName: "Aurdino (UNO)",
+    productUrl: "https://m.media-amazon.com/images/I/51FKBDpXEqL.jpg",
+    productQty: 0,
+    productPrice: 560,
+    productTotal: 0 },
+
+  {
+    productName: "NIC",
+    productUrl: "https://m.media-amazon.com/images/I/81xhxQ7qCbL._SX679_.jpg",
+    productQty: 0,
+    productPrice: 210,
+    productTotal: 0 },
+
+  {
+    productName: "Cabinet",
+    productUrl: "https://m.media-amazon.com/images/I/61-0HSm+zeL._SL1200_.jpg",
+    productQty: 0,
+    productPrice: 2990,
+    productTotal: 0 },
+
+  {
+    productName: "Motherboard",
+    productUrl: "https://m.media-amazon.com/images/I/818fDro1x9L._SL1500_.jpg",
+    productQty: 0,
+    productPrice: 7980,
+    productTotal: 0 },
+
+  {
+    productName: "Router",
+    productUrl: "https://m.media-amazon.com/images/I/51p2d9NML5L._SL1000_.jpg",
+    productQty: 0,
+    productPrice: 3450,
+    productTotal: 0 },
+
+  {
+    productName: "Ram",
+    productUrl: "https://m.media-amazon.com/images/I/51eb3VQyt4L._SL1000_.jpg",
+    productQty: 0,
+    productPrice: 13150,
+    productTotal: 0 }]);
+
+
+  const totalQtyAndPrice = () => {
+    let qty = 0,
+    price = 0;
+    data.map(
+    (item) => (
+    qty = qty + item.productQty, price = price + item.productTotal));
+
+
+    setTotal({ totalQty: qty, totalPrice: price });
+  };
+  const [eComList, setEComList] = React.useState(data);
+  const Inc = index => {
+    data[index].productQty = data[index].productQty + 1;
+    data[index].productTotal =
+    data[index].productPrice * data[index].productQty;
+    setEComList(data);
+    setdata(eComList);
+    totalQtyAndPrice();
+  };
+  const Drc = index => {
+    if (data[index].productQty > 0) {
+      data[index].productQty = data[index].productQty - 1;
+      data[index].productTotal =
+      data[index].productPrice * data[index].productQty;
+      setEComList(data);
+      setdata(eComList);
+      totalQtyAndPrice();
+    }
+  };
+  const cartList = index => {
+    if (data[index].productQty > 0) {
+      return /*#__PURE__*/(
+        React.createElement("tr", null, /*#__PURE__*/
+        React.createElement("td", null, /*#__PURE__*/React.createElement("span", { class: "text-truncate" }, data[index].productName), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/
+        React.createElement("button", { class: "btn btn-sm bg-colord colorl", onClick: () => {Inc(index);} }, "+"), " ", data[index].productQty, " ", /*#__PURE__*/React.createElement("button", { class: "btn btn-sm bg-colord colorl", onClick: () => {Drc(index);} }, "-")), /*#__PURE__*/
+        React.createElement("td", { class: "text-right w-25" }, data[index].productTotal, ".00")));
+
+
+    }
+  };
+  const billing = () => {
+    let grossamount = 0;
+    let taxamount = 0;
+    data.map(
+    (item) =>
+    grossamount = grossamount + item.productTotal);
+
+
+    let tempx = grossamount;
+    grossamount = grossamount.toFixed(2);
+    taxamount = grossamount * 18 / 100;
+    let tempy = taxamount;
+    taxamount = (Math.round((taxamount + Number.EPSILON) * 100) / 100).toFixed(2);
+    totalamount = tempx + tempy;
+    if (cartItems > 0) {
+      totalamount = totalamount + 40;
+    }
+    totalamount = totalamount.toFixed(2);
+    return /*#__PURE__*/(
+      React.createElement("table", null, /*#__PURE__*/
+      React.createElement("tr", null, /*#__PURE__*/
+      React.createElement("td", null, "Gross Amount"), /*#__PURE__*/
+      React.createElement("td", null, ":"), /*#__PURE__*/
+      React.createElement("td", { class: "w-25 text-right" }, grossamount)), /*#__PURE__*/
+
+      React.createElement("tr", null, /*#__PURE__*/
+      React.createElement("td", null, "Tax Amount (18%)"), /*#__PURE__*/
+      React.createElement("td", null, ":"), /*#__PURE__*/
+      React.createElement("td", { class: "w-25 text-right" }, taxamount)), /*#__PURE__*/
+
+      React.createElement("tr", null, /*#__PURE__*/
+      React.createElement("td", null, "Shipping Amount"), /*#__PURE__*/
+      React.createElement("td", null, ":"), /*#__PURE__*/
+      React.createElement("td", { class: "w-25 text-right" }, "40.00"))));
+
+
+
+  };
+  const cartCounter = () => {
+    data.map(item => {
+      if (item.productQty > 0) {
+        cartItems = cartItems + 1;
+      }
+    });
+    return cartItems;
+  };
   return /*#__PURE__*/(
     React.createElement("div", null, /*#__PURE__*/
     React.createElement("div", null, /*#__PURE__*/
@@ -30,7 +164,7 @@ function CheckLogin() {
 
 
     React.createElement("div", { class: "form-inline my-2 my-lg-0" }, /*#__PURE__*/
-    React.createElement("a", { href: "", class: "colord mr-4" }, /*#__PURE__*/React.createElement("b", null, "Cart")), /*#__PURE__*/
+    React.createElement("a", { href: "#", class: "colord mr-4" }, /*#__PURE__*/React.createElement("b", null, "Cart ", /*#__PURE__*/React.createElement("span", { class: "badge bg-colord colorl" }, cartCounter()))), /*#__PURE__*/
     React.createElement("button", { class: "btn bg-colord colorl btn-md" }, "Logout")))))), /*#__PURE__*/
 
 
@@ -42,80 +176,44 @@ function CheckLogin() {
     React.createElement("h5", null, "All Products"), /*#__PURE__*/
     React.createElement("div", { class: "m-3 row" }, /*#__PURE__*/
     React.createElement("div", { class: "col-md-9" }, /*#__PURE__*/
-    React.createElement("div", { class: "row" }, /*#__PURE__*/
+    React.createElement("div", { class: "row" },
+    data.map((item, index) => /*#__PURE__*/
     React.createElement("div", { class: "card col-md-3 m-4" }, /*#__PURE__*/
     React.createElement("div", { class: "card-image" }, /*#__PURE__*/
-    React.createElement("img", { src: "https://m.media-amazon.com/images/I/51FKBDpXEqL.jpg", height: "180", width: "100%" })), /*#__PURE__*/
+    React.createElement("img", { src: item.productUrl, height: "180", width: "100%" })), /*#__PURE__*/
 
-    React.createElement("div", { class: "card-header text-center colord bg-transparent" }, "Aurdino (UNO)"), /*#__PURE__*/
+    React.createElement("div", { class: "card-header text-center colord bg-transparent" },
+    item.productName, /*#__PURE__*/
+    React.createElement("br", null), "Price : ",
+    item.productPrice, ".00"), /*#__PURE__*/
 
+    React.createElement("button", { class: "btn bg-colord colorl mb-2", onClick: () => {Inc(index);} }, "Add To Cart"))))), /*#__PURE__*/
 
-    React.createElement("button", { class: "btn bg-colord colorl mb-2" }, "Add To Cart")), /*#__PURE__*/
-
-
-    React.createElement("div", { class: "card col-md-3 m-4" }, /*#__PURE__*/
-    React.createElement("div", { class: "card-image" }, /*#__PURE__*/
-    React.createElement("img", { src: "https://m.media-amazon.com/images/I/81xhxQ7qCbL._SX679_.jpg", height: "180", width: "100%" })), /*#__PURE__*/
-
-    React.createElement("div", { class: "card-header text-center colord bg-transparent" }, "NIC"), /*#__PURE__*/
-
-
-    React.createElement("button", { class: "btn bg-colord colorl mb-2" }, "Add To Cart")), /*#__PURE__*/
-
-
-    React.createElement("div", { class: "card col-md-3 m-4" }, /*#__PURE__*/
-    React.createElement("div", { class: "card-image" }, /*#__PURE__*/
-    React.createElement("img", { src: "https://m.media-amazon.com/images/I/61-0HSm+zeL._SL1200_.jpg", height: "180", width: "100%" })), /*#__PURE__*/
-
-    React.createElement("div", { class: "card-header text-center colord bg-transparent" }, "Cabinet"), /*#__PURE__*/
-
-
-    React.createElement("button", { class: "btn bg-colord colorl mb-2" }, "Add To Cart")), /*#__PURE__*/
-
-
-    React.createElement("div", { class: "card col-md-3 m-4" }, /*#__PURE__*/
-    React.createElement("div", { class: "card-image" }, /*#__PURE__*/
-    React.createElement("img", { src: "https://m.media-amazon.com/images/I/818fDro1x9L._SL1500_.jpg", height: "180", width: "100%" })), /*#__PURE__*/
-
-    React.createElement("div", { class: "card-header text-center colord bg-transparent" }, "Motherboard"), /*#__PURE__*/
-
-
-    React.createElement("button", { class: "btn bg-colord colorl mb-2" }, "Add To Cart")), /*#__PURE__*/
-
-
-    React.createElement("div", { class: "card col-md-3 m-4" }, /*#__PURE__*/
-    React.createElement("div", { class: "card-image" }, /*#__PURE__*/
-    React.createElement("img", { src: "https://m.media-amazon.com/images/I/51p2d9NML5L._SL1000_.jpg", height: "180", width: "100%" })), /*#__PURE__*/
-
-    React.createElement("div", { class: "card-header text-center colord bg-transparent" }, "Router"), /*#__PURE__*/
-
-
-    React.createElement("button", { class: "btn bg-colord colorl mb-2" }, "Add To Cart")))), /*#__PURE__*/
 
 
 
     React.createElement("div", { class: "col-md-3 mt-4" }, /*#__PURE__*/
     React.createElement("div", { class: "rounded-2 card p-2 colord" }, /*#__PURE__*/
     React.createElement("h5", null, "Cart"), /*#__PURE__*/
-    React.createElement("div", { class: "cart-menu border border-" }), /*#__PURE__*/
+    React.createElement("div", { class: "border" }), /*#__PURE__*/
+    React.createElement("div", { class: "cart-menu" }, /*#__PURE__*/
+    React.createElement("table", null,
+    data.map((item, index) =>
+    cartList(index)))), /*#__PURE__*/
 
 
-    React.createElement("div", { class: "border-0 border-bottom-1" }, /*#__PURE__*/
+
+    React.createElement("div", { class: "border" }), /*#__PURE__*/
+    React.createElement("div", { class: "" },
+    billing()), /*#__PURE__*/
+
+    React.createElement("div", { class: "border" }), /*#__PURE__*/
+    React.createElement("div", { class: "" }, /*#__PURE__*/
     React.createElement("table", null, /*#__PURE__*/
     React.createElement("tr", null, /*#__PURE__*/
-    React.createElement("td", null, "Gross Amount"), /*#__PURE__*/
-    React.createElement("td", null, ":"), /*#__PURE__*/
-    React.createElement("td", null, "0")), /*#__PURE__*/
-
-    React.createElement("tr", null, /*#__PURE__*/
-    React.createElement("td", null, "Tax Amount"), /*#__PURE__*/
-    React.createElement("td", null, ":"), /*#__PURE__*/
-    React.createElement("td", null, "0")), /*#__PURE__*/
-
-    React.createElement("tr", null, /*#__PURE__*/
-    React.createElement("td", null, "Shipping Amount"), /*#__PURE__*/
-    React.createElement("td", null, ":"), /*#__PURE__*/
-    React.createElement("td", null, "0"))))))))), /*#__PURE__*/
+    React.createElement("td", null, "Total Amount"), /*#__PURE__*/
+    React.createElement("td", { class: "w-25 text-right" }, ":"), /*#__PURE__*/
+    React.createElement("td", { class: "w-25 text-right" }, totalamount))))))))), /*#__PURE__*/
 
 
 
@@ -133,5 +231,5 @@ function CheckLogin() {
 }
 
 ReactDOM.render( /*#__PURE__*/
-React.createElement(CheckLogin, null),
+React.createElement(Cartx, null),
 document.getElementById('root'));
